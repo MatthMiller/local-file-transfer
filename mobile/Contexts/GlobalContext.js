1e00102858100397cca2a1abb201fd36a9a84d8e
MatthMiller <matheus.monteiro.genesis@gmail.com>
2024-05-15 10:59:03 -0300

    :sparkles: Gerando UUID do dispositivo móvel

--- mobile/Contexts/GlobalContext.js
+++ mobile/Contexts/GlobalContext.js
@@ -1,3 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Crypto from 'expo-crypto';
+import * as Device from 'expo-device';
 import React from 'react';
 import { ToastAndroid } from 'react-native';
 import { io } from 'socket.io-client';
@@ -5,6 +8,7 @@ import { io } from 'socket.io-client';
 export const GlobalContext = React.createContext();
 
 export const GlobalStorage = ({ children }) => {
+  const [deviceUUID, setDeviceUUID] = React.useState(null);
   const [socketState, setSocketState] = React.useState(null);
   const [headerStatus, setHeaderStatus] = React.useState({
     isConnected: false,
@@ -14,19 +18,22 @@ export const GlobalStorage = ({ children }) => {
   const attemptToConnect = (ip) => {
     const socket = io(`http://${ip}:3000`);
 
-    socket.on('connect', () => handleConnected(ip));
+    socket.on('connect', () => handleConnected(ip, socket));
     socket.on('connect_error', (error) =>
       handleConnectedError(ip, error, socket)
     );
   };
 
-  const handleConnected = (ip) => {
+  const handleConnected = (ip, socket) => {
     console.log(`Conectado ao servidor ${ip}`);
-    // ToastAndroid.show(`Connected to ${ip}`, ToastAndroid.SHORT);
+
     setHeaderStatus({
       isConnected: true,
       ip: ip,
     });
+    socket.emit('setPlatform', JSON.stringify({ modelName: Device.modelName }));
+    // Expo get device info
+    // socket.emit('setPlatform', JSON.stringify(deviceInfo));
   };
 
   const handleConnectedError = (ip, error, socket) => {
@@ -39,6 +46,21 @@ export const GlobalStorage = ({ children }) => {
     });
 
     socket.disconnect();
+  };
+
+  React.useEffect(() => {
+    configureDeviceUUID();
+  }, []);
+
+  const configureDeviceUUID = async () => {
+    const uuidStorage = await AsyncStorage.getItem('deviceUUID');
+    // console.log('uuidStorage:', uuidStorage);
+    if (uuidStorage === null) {
+      const uuid = Crypto.randomUUID();
+      await AsyncStorage.setItem('deviceUUID', uuid);
+    } else {
+      setDeviceUUID(uuidStorage);
+    }
   };
 
   return (
